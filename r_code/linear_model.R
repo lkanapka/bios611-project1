651662c398cf0206dd0e710795394cbbe560bfb3
Lauren Kanapka <lkanapka@email.unc.edu>
2020-10-06 17:08:53 -0400

    Add code to fit linear models and save p-values.

---
+++ r_code/linear_model.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+# Read in data
+storms <- read_csv("derived_data/storms.csv")
+
+# Code to fit linear regression models and save p-values
+
+# Create empty p-values data set
+pVal <- c("P-value for Time", NA, NA, NA, NA, NA)
+
+# Number of storms over time
+countYr<- storms %>% group_by(year) %>%
+          summarize(count=n(),countCyclone=sum(cycloneFlg), 
+                    countHur=sum(hurFlg), countMajor=sum(majorFlg),
+                    meanWind=mean(wind))
+countYr$time<- countYr$year-1968
+modelA <- lm(count ~ time, data=countYr)
+modelASum<-summary(modelA)
+pVal[2, 1]<- as.character(round(modelASum$coefficients[2,4], 2))
+
+# Number of cyclones over time
+modelB <- lm(countCyclone ~ time, data=countYr)
+modelBSum<-summary(modelB)
+pVal[3, 1]<- as.character(round(modelBSum$coefficients[2,4], 2))
+
+# Number of hurricanes over time
+modelC <- lm(countHur ~ time, data=countYr)
+modelCSum<-summary(modelC)
+pVal[4, 1]<- as.character(round(modelCSum$coefficients[2,4], 3))
+
+# Number of major hurricanes over time
+modelD <- lm(count ~ time, data=countYr)
+modelDSum<-summary(modelD)
+pVal[5, 1]<- as.character(round(modelDSum$coefficients[2,4], 3))
+
+# Average wind speed over time
+modelE <- lm(meanWind ~ time, data=countYr)
+modelESum <- summary(modelE)
+pVal[6, 1]<- "<0.001"
+
+# Output p-values to derived data
+write.csv(pVal, "derived_data/pValues.csv", row.names = F)
